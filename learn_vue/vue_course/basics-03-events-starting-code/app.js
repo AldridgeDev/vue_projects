e660e94483fb8a2ea90e6f29676aa5cc3f0074d1
AldridgeDev <aldridge.developer@gmail.com>
2020-10-28 07:33:43 -0400

    next steps

--- learn_vue/vue_course/basics-03-events-starting-code/app.js
+++ learn_vue/vue_course/basics-03-events-starting-code/app.js
@@ -2,6 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
+      confirmedName: ''
     };
   },
   methods: {
@@ -10,6 +12,16 @@ const app = Vue.createApp({
     },
     subtract(num) {
      this.counter = this.counter - num;
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    submitForm(event) {
+      // event.preventDefault();
+      alert('Submitted!');
+    },
+    confirmName() {
+      this.confirmedName = this.name;
     }
   }
 });
